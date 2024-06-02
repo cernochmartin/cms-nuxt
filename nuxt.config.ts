358a76c893255c2569fe4b061f1e0ce226cb3b20
cernochmartin <cernochm@outlook.cz>
2024-06-02 19:08:23 +0200

    fix: html lang to en-US

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
         { src: 'https://kit.fontawesome.com/f6333c832d.js', defer: true, crossorigin: 'anonymous' },
       ],
       htmlAttrs: {
-        lang: 'cs-CZ'
+        lang: 'en-US'
       }
     }
   }
