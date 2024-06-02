48244183494fde525348e5b3a8f040f5389f2c22
cernochmartin <cernochm@outlook.cz>
2024-06-02 19:08:53 +0200

    fix: deletion of fontawesome integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,9 +17,6 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      script: [
-        { src: 'https://kit.fontawesome.com/f6333c832d.js', defer: true, crossorigin: 'anonymous' },
-      ],
       htmlAttrs: {
         lang: 'en-US'
       }
